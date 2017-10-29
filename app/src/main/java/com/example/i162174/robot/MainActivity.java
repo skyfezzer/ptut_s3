3c4f9aba74653b72ebfd2d57b539623fac98f16e
Alexis RAGOT <alexisragot77@gmail.com>
2017-10-29 19:11:25 +0100

    Android app upload
    Base application, with Main Scenario and Credits.

---
+++ app/src/main/java/com/example/i162174/robot/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.i162174.robot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView logoImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logoImageView = (ImageView)findViewById(R.id.logoImageView);
+        logoImageView.setImageResource(R.drawable.logo_equipe);
+
+
+        new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {}
+                Intent intent = new Intent(MainActivity.this, ScenarioActivity.class);
+                startActivity(intent);
+            }}.start();
+
+    }
+
+}
+
