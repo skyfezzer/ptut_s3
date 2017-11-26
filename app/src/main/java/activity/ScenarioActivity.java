443b5452f5d4c0ca5100f9a949856e7e2b1e6e66
xNarCiik <damienlegagnoux@live.fr>
2017-11-26 22:46:13 +0100

    Ajout QR Code
    - Ajout du QR Code
    - Lance le market si application non installé
    - Gestion de l'annulation

--- app/src/main/java/activity/ScenarioActivity.java
+++ app/src/main/java/activity/ScenarioActivity.java
@@ -1,6 +1,11 @@
 package activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.i162174.robot.R;
 
@@ -11,6 +16,35 @@ public class ScenarioActivity extends ActivityAvecMenu {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario);
         initialisationMenu();
+
+        ImageView img_qrcode = (ImageView) findViewById(R.id.img_qrcode);
+        img_qrcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+
+                    startActivityForResult(intent, 0);
+                }catch(Exception e){
+                    Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+                    Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+                    startActivity(marketIntent);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Toast.makeText(this, data.getStringExtra("SCAN_RESULT"), Toast.LENGTH_SHORT).show();
+        }
+        if(resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "Action annulée", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
